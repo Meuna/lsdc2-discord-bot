6bd12cf1f4de3aa45ef026e705e053a6deb2e06c
Thomas Bany <mun.bany@gmail.com>
2022-08-13 16:02:08 +0200

    Improved build scripts

--- update.sh
+++ update.sh
@@ -1,12 +1,24 @@
 # /bin/bash
+
+find_lambda() {
+    python3 -c "import sys, json; key='FunctionName'; f=next(f[key] for f in json.load(sys.stdin)['Functions'] if '$1' in f[key]); print(f)"
+}
+
+lambda_list=$(aws lambda list-functions)
+frontend_lambda=$(echo $lambda_list | find_lambda "Lsdc2CdkStack-discordBotFrontend")
+backend_lambda=$(echo $lambda_list | find_lambda "Lsdc2CdkStack-discordBotBackend")
+
+zip --junk-paths build/frontend.zip build/frontend
+zip --junk-paths build/backend.zip build/backend
+
 aws lambda update-function-code \
-    --function-name $FRONTEND_LAMBDA \
-    --zip-file fileb://$(pwd)/frontend.zip \
+    --function-name $frontend_lambda \
+    --zip-file fileb://$(pwd)/build/frontend.zip \
     --no-cli-pager \
     > /dev/null
 
 aws lambda update-function-code \
-    --function-name $BACKEND_LAMBDA \
-    --zip-file fileb://$(pwd)/backend.zip \
+    --function-name $backend_lambda \
+    --zip-file fileb://$(pwd)/build/backend.zip \
     --no-cli-pager \
     > /dev/null
