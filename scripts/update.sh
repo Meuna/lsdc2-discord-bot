5b7d4e982cdddc30e99c5f18b57d3ec84d6d6d98
Thomas Bany <mun.bany@gmail.com>
2025-02-23 12:24:58 +0100

    Fixl update.sh in its new folder

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,4 +1,7 @@
-# /bin/bash
+#!/bin/bash
+
+script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+src_dir=$script_dir/..
 
 find_lambda() {
     python3 -c "import sys, json; key='FunctionName'; f=next(f[key] for f in json.load(sys.stdin)['Functions'] if '$1' in f[key]); print(f)"
@@ -10,12 +13,12 @@ backend_lambda=$(echo $lambda_list | find_lambda "Lsdc2CdkStack-discordBotBacken
 
 aws lambda update-function-code \
     --function-name $frontend_lambda \
-    --zip-file fileb://$(pwd)/build/frontend.zip \
+    --zip-file fileb://$src_dir/frontend.zip \
     --no-cli-pager \
     > /dev/null
 
 aws lambda update-function-code \
     --function-name $backend_lambda \
-    --zip-file fileb://$(pwd)/build/backend.zip \
+    --zip-file fileb://$src_dir/backend.zip \
     --no-cli-pager \
     > /dev/null
